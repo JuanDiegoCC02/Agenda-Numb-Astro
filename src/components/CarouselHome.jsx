fd0aab20a8f009777704087b70501d7c406d99ad
JuanDiegoCC02 <dcorella2324@gmail.com>
2025-10-23 18:39:59 -0600

    Updating carrousel Home structure.

--- src/components/CarouselHome.jsx
+++ src/components/CarouselHome.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import "../styles/CarouselHome.css";
-import Consistency from '../imagenes/Constancy.png';
-import Perseverance from '../imagenes/Perseverance.png';
-import Focus from '../imagenes/Focus.png';
+import homePagNA from '../imagenes/homePagNA.PNG'
+import formTPagNA from '../imagenes/formTPagNA.PNG'
+import getTPagNA from '../imagenes/getTPagNA.PNG'
+import mapPagNA from '../imagenes/mapPagNA.PNG'
+import lootPagNA from '../imagenes/lootPagNA.PNG'
+import profilePagNa from '../imagenes/profilePagNa.PNG'
+
 
 function CarouselHome() {
       const images = [
-        { src: Consistency, title: "Constancy", desc: "Build habits with small daily steps." },
-        { src: Perseverance, title: "Perseverance", desc: "Stay strong even when it's hard." },
-        { src: Focus, title: "Focus", desc: "Direct your energy to what truly matters." }
+        { src: homePagNA, title: "🌌 Welcome to Numb Astro", desc: "Your cosmic space to organize, focus, and grow. Transform your tasks into a stellar journey toward your goals. Plan with clarity, cultivate healthy habits, and find balance every day." },
+        { src: formTPagNA, title: "Create a Task", desc: "Create a task to generate new habit and embark on a new stellar journey." },
+        { src: getTPagNA, title: "Update or complete a Task", desc: "Uptading or Marking a task to continue moving forward on the star journey." },
+        { src: mapPagNA, title: "Discover Stars", desc: "Discover stars on the star map by completing tasks and progressing through your habits.." },
+        { src: lootPagNA, title: "Get Stellar Memories", desc: "Obtain star memories by advancing on the star map and discovering new memories left by the stars.." },
+        { src: profilePagNa, title: "Profile", desc: "Explain profile." }
       ];
     
       const [index, setIndex] = useState(0);
